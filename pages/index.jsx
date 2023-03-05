724d4b512039e0c777e32afe1d4c4b580fdf9ebf
쏘냐, Sonya <66970178+onlinekkabchi@users.noreply.github.com>
2023-03-05 20:34:01 +0900

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my Next.js app.</p>
+    </div>
+  );
+};
+
+export default HomePage;
